4e2c9fddd431ff7677b0c2097c86ad1e64980823
Juliette Curran <juliettecurran@hotmail.com>
2021-06-14 17:10:45 +0100

    implement insert reviews function

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -5,3 +5,30 @@ exports.createCategoryData = (array) => {
     return [category.slug, category.description];
   });
 };
+
+exports.createUserData = (array) => {
+  return array.map((user) => {
+    return [user.username, user.avatar_url, user.name];
+  });
+};
+
+exports.createReviewData = (array) => {
+  return array.map((review) => {
+    return [
+      review.title,
+      review.designer,
+      review.owner,
+      review.review_body,
+      review.created_at,
+      review.category,
+      review.votes,
+      review.review_img_url,
+    ];
+  });
+};
+
+/* exports.createCommentData = (array) => {
+    return array.map((comment) => {
+      return [comment.comment_id, comment.author, comment.review_id, ];
+    });
+  }; */
