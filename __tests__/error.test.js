52c620cd65f0b53480e311ddda488db555e73396
two-wheels01 <mail.name@mailbox.org>
2021-06-17 18:18:50 +0100

    update error tests

--- __tests__/error.test.js
+++ __tests__/error.test.js
@@ -4,6 +4,9 @@ const testData = require('../db/data/test-data/index.js');
 const seed = require('../db/seeds/seed.js');
 const request = require('supertest');
 
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
 describe('invalid url path', () => {
   test('status:404 - Not Found', () => {
     return request(app)
@@ -94,6 +97,46 @@ describe('PATCH /api/reviews/:review_id', () => {
         expect(body).toEqual({
           msg: 'Bad Request',
         });
+      });
+  });
+  it('400: Bad Request - invalid body (too many keys)', () => {
+    const incVoteBy = { inc_votes: 1, name: 'Mitch' };
+
+    return request(app)
+      .patch('/api/reviews/2')
+      .send(incVoteBy)
+      .expect(400)
+      .then(({ body }) => {
+        expect(body).toEqual({
+          msg: 'Bad Request',
+        });
+      });
+  });
+});
+
+describe('GET /api/reviews', () => {
+  it('400: Invalid sort query', () => {
+    return request(app)
+      .get('/api/reviews?sort_by=elephant')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe('Invalid sort query');
+      });
+  });
+  it('400: Invalid order query', () => {
+    return request(app)
+      .get('/api/reviews?sort_by=review_id&order=ascending')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe('Invalid order query');
+      });
+  });
+  it('400: Invalid category query', () => {
+    return request(app)
+      .get('/api/reviews?category=hitandrun')
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe('category does not exist');
       });
   });
 });
