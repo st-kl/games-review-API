bd798445d17032127829c89b30f09f0546a569ef
two-wheels01 <mail.name@mailbox.org>
2021-06-17 18:19:58 +0100

    update model

--- models/categories.model.js
+++ models/categories.model.js
@@ -1,13 +1,14 @@
 const db = require('../db/connection');
+const { checkExists } = require('../db/utils/query-check');
 
-exports.selectCategories = () => {
-  return db.query('SELECT * FROM categories;').then((result) => result.rows);
+exports.selectCategories = async () => {
+  const result = await db.query('SELECT * FROM categories;');
+  return result.rows;
 };
 
-exports.selectReviewById = (reviewId) => {
-  return db
-    .query(
-      `
+exports.selectReviewById = async (reviewId) => {
+  const res = await db.query(
+    `
   SELECT 
     reviews.*,
     COUNT(comments.comment_id)
@@ -21,54 +22,75 @@ exports.selectReviewById = (reviewId) => {
     reviews.review_id = $1
   GROUP BY
     reviews.review_id;`,
-      [reviewId]
-    )
-    .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: 'Not Found - ID does not exist',
-        });
-      }
-      return res.rows;
+    [reviewId]
+  );
+  if (res.rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: 'Not Found - ID does not exist',
     });
+  }
+  return res.rows;
 };
 
-exports.updateReviewById = (reviewId, inc_votes) => {
-  if (!inc_votes) {
+exports.updateReviewById = async (reviewId, inc_votes) => {
+  if (!inc_votes || bodyLength > 1) {
     return Promise.reject({
       status: 400,
       msg: 'Bad Request',
     });
   }
 
-  return db
-    .query(
-      `
+  await db.query(
+    `
   UPDATE reviews SET votes = votes + $1 WHERE review_id = $2;
   `,
-      [inc_votes, reviewId]
-    )
-    .then(() => {
-      return db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [
-        reviewId,
-      ]);
-    })
-    .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: 'Not Found - ID does not exist',
-        });
-      }
-      return res.rows[0];
+    [inc_votes, reviewId]
+  );
+  const res = await db.query(`SELECT * FROM reviews WHERE review_id = $1;`, [
+    reviewId,
+  ]);
+  if (res.rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: 'Not Found - ID does not exist',
     });
+  }
+  return res.rows[0];
 };
 
-exports.selectReviews = () => {
-  return db
-    .query(
-      `
+exports.selectReviews = async (
+  sort_by = 'created_at',
+  order = 'desc',
+  category
+) => {
+  // validate sort_by query
+  if (
+    ![
+      'owner',
+      'title',
+      'review_id',
+      'category',
+      'review_img_url',
+      'created_at',
+      'votes',
+      'comment_count',
+    ].includes(sort_by)
+  ) {
+    return Promise.reject({ status: 400, msg: 'Invalid sort query' });
+  }
+
+  // validate order query
+  if (!['asc', 'desc'].includes(order)) {
+    return Promise.reject({ status: 400, msg: 'Invalid order query' });
+  }
+
+  // validate categories
+  await checkExists('reviews', 'category', category);
+
+  // query arguments
+  const queryValues = [];
+  let queryStr = `
   SELECT 
   reviews.owner,
   reviews.title, 
@@ -76,15 +98,27 @@ exports.selectReviews = () => {
   reviews.category,
   reviews.review_img_url,
   reviews.created_at,
-  COUNT(comments.comment_id)::INTEGER AS comment_count
+  COUNT(comments.comment_id)::INT AS comment_count
   FROM 
     comments
   LEFT JOIN
     reviews
   ON
-    comments.review_id = reviews.review_id
-  GROUP BY
-    reviews.review_id;`
-    )
-    .then((result) => result.rows);
+    comments.review_id = reviews.review_id`;
+
+  // add colour to query values if provided and extend query string
+  if (category) {
+    queryValues.push(category);
+    queryStr += ` WHERE reviews.category = $1`;
+  }
+
+  //extend query string with group by
+  queryStr += ` GROUP BY reviews.review_id`;
+
+  // extend query string with order by
+  queryStr += ` ORDER BY reviews.${sort_by} ${order};`;
+
+  const result = await db.query(queryStr, queryValues);
+
+  return result.rows;
 };
