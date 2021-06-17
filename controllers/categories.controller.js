b2827b73573f1905ef9a2e07789f8cb9f438238b
two-wheels01 <mail.name@mailbox.org>
2021-06-17 18:20:57 +0100

    update controller

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -24,13 +24,17 @@ exports.getReviewById = (req, res, next) => {
 exports.patchReviewById = (req, res, next) => {
   const reviewId = req.params.review_id;
   const { inc_votes } = req.body;
-  updateReviewById(reviewId, inc_votes)
+  const bodyLength = Object.keys(req.body).length;
+  updateReviewById(reviewId, inc_votes, bodyLength)
     .then((reviews) => res.status(200).send({ reviews }))
     .catch(next);
 };
 
 exports.getReviews = (req, res, next) => {
-  selectReviews().then((reviews) => {
-    res.status(200).send({ reviews });
-  });
+  const { sort_by, order, category } = req.query;
+  selectReviews(sort_by, order, category)
+    .then((reviews) => {
+      res.status(200).send({ reviews });
+    })
+    .catch(next);
 };
