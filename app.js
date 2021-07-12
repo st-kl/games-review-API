7b1bad24e89c5c2677d160c029728767e109127a
two-wheels01 <mail.name@mailbox.org>
2021-07-12 12:24:44 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const {
 } = require('./errors/app.errors');
 const apiRouter = require('./routes/api.router');
 const app = express();
+const cors = require('/cors');
 
+app.use(cors());
 app.use(express.json());
 
 // ------- Routers -------
